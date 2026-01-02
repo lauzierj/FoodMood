1afdb00718af0376f317caadd82814f90f3c0678
Jacob Lauzier <jacob.lauzier@machinemetrics.com>
2026-01-02 11:54:57 -0500

    manifest base

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      base: "/FoodMood/",
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
